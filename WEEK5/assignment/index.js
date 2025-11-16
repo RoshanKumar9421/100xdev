951c6d12ce1a0c3468ca2e9622e9c2e0f2d39baa
RoshanKumar9421 <yadavroshankumar8271@gmail.com>
2025-11-17 02:04:34 +0530

    first commit

---
+++ WEEK5/assignment/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors);
+
+app.post("/sum", function(req, res){
+    const a= parseInt(req.query.a);
+    const b= parseInt(req.query.b);
+
+    res.json({
+        ans:a+b
+    })
+});
+
+app.listen(3000);
